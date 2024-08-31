0fe66a08049894f45926adce827de5c9289e1197
Alencruser <alencruserxx@hotmail.fr>
2024-08-31 10:54:18 +0200

    create a script to use nodemon server side, added thhe mysql connection and prepared the home page request

--- server/index.js
+++ server/index.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mysql = require("mysql");
+const security = require("./security.js");
+
+const connection = mysql.createConnection({
+    host: security.host,
+    user: security.user,
+    password: security.password,
+})
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('Hello from our server!')
+    connection.connect();
+    // On selectionne les 10 categories qui ont eu les sujets les plus actifs recemment
+    connection.query("SELECT * FROM Category INNER JOIN Subject on Category.id = Subject.category_id ORDER BY Subject.modified_at DESC LIMIT 10", (err, results) => {
+        if(err)return console.error(err);
+        res.send(results);
+    });
+    connection.end();
+    res.send([]);
 })
+
+app.post("/category", (req,res) => {
+    console.log(req);
+});
+
 
 app.listen(8080, () => {
     console.log('server listening on port  8080')
